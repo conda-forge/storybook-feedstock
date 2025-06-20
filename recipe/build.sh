a807a260f5beb7031b9cf2f61aec00a6c9ea27ae
Daniel Nachun <daniel.nachun@gmail.com>
2025-06-20 10:24:09 -0700

    fix build

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,6 +5,7 @@ set -o xtrace -o nounset -o pipefail -o errexit
 # Create package archive and install globally
 npm pack --ignore-scripts
 npm install -ddd \
+    --no-bin-links \
     --global \
     --build-from-source \
     ${SRC_DIR}/${PKG_NAME}-${PKG_VERSION}.tgz
@@ -13,8 +14,15 @@ npm install -ddd \
 pnpm install
 pnpm-licenses generate-disclaimer --prod --output-file=third-party-licenses.txt
 
+mkdir -p ${PREFIX}/bin
+tee ${PREFIX}/bin/storybook << EOF
+#!/bin/sh
+\${CONDA_PREFIX}/lib/node_modules/storybook/bin/index.cjs "\$@"
+EOF
+chmod +x ${PREFIX}/bin/storybook
+
 tee ${PREFIX}/bin/storybook.cmd << EOF
-call %CONDA_PREFIX%\bin\node %CONDA_PREFIX%\bin\storybook %*
+call %CONDA_PREFIX%\bin\node %CONDA_PREFIX%\lib\node_modules\storybook\bin\index.cjs %*
 EOF
 
 # Delete vendored esbuild so package is noarch
